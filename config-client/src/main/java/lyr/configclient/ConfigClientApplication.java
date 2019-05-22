83916ede321c50c731a4bbfb6a915737d71b04d6
lyr <270809304@qq.com>
2019-05-22 17:07:03 +0800

    修改项目结构，完成配置中心配置，实现半自动热跟新配置

--- config-client/src/main/java/lyr/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/lyr/configclient/ConfigClientApplication.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
-@EnableDiscoveryClient
 @RefreshScope
 public class ConfigClientApplication {
 
